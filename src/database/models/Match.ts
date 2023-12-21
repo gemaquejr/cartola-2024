9d5b1e04e54be99fb1b61b9b4b2a682e2637011e
gemaquejr <gemaquejr@hotmail.com>
2023-12-20 23:26:17 -0300

    adiciona models de Match
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/database/models/Match.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '../../db'
+
+import Team from './Team';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+ Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+ Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
