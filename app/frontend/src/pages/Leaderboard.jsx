5fbc86587a7511b663a8ab4af9b79e72f7475cea
gemaquejr <gemaquejr@hotmail.com>
2023-12-22 12:04:04 -0300

    adiciona Leaderboard
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ app/frontend/src/pages/Leaderboard.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import LeaderboardTable from '../components/LeaderboardTable';
+import TableFilter from '../components/TableFilter';
+
+const Leaderboard = () => {
+  const [currentFilter, setCurrentFilter] = useState('Classificação Geral');
+
+  return (
+    <>
+      <div className="classification-handlers score-board-table-section">
+        <TableFilter
+          currentFilter={ currentFilter }
+          setCurrentFilter={ setCurrentFilter }
+        />
+      </div>
+      <LeaderboardTable
+        currentFilter={ currentFilter }
+        setCurrentFilter={ setCurrentFilter }
+      />
+    </>
+  );
+};
+
+export default Leaderboard;
