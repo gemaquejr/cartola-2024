f1179db2e24a5a5c8af71bdbb2fe94af94a8254c
gemaquejr <gemaquejr@hotmail.com>
2023-12-22 12:03:33 -0300

    adiciona TableFilter
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ app/frontend/src/components/TableFilter.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GamerFilter = ({ currentFilter, setCurrentFilter }) => {
+  const handleCurrentFilter = () => {
+    const selectedFilter = document.getElementById('classification-filter').value;
+    setCurrentFilter(selectedFilter);
+  };
+
+  return (
+    <form>
+      <label htmlFor="classification-filter">
+        Partidas:
+        <select
+          id="classification-filter"
+          defaultValue={ currentFilter }
+          data-testid="score_boarding__classification_filter"
+        >
+          <option>Classificação Geral</option>
+        </select>
+      </label>
+      <button
+        data-testid="score_boarding__classification_filter_button"
+        type="button"
+        onClick={ () => handleCurrentFilter() }
+      >
+        Buscar
+      </button>
+    </form>
+  );
+};
+
+GamerFilter.propTypes = ({
+  currentFilter: PropTypes.string,
+  setCurrentFilter: PropTypes.func,
+}).isRequired;
+
+export default GamerFilter;
