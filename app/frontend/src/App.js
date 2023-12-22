f0ad59ffaebb5c68765ac3b38a7a8012f76bbdad
gemaquejr <gemaquejr@hotmail.com>
2023-12-22 20:08:11 -0300

    adiciona configuracao react-router-dom
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+
+import './App.css'
+import Leaderboard from './pages/Leaderboard';
+
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        </header>
+  return (
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route exact path="/" element={ <Navigate to="/leaderboard" /> } />
+        </Routes>
       </div>
-    );
+    </Router>
+  );
 }
+
 export default App;
