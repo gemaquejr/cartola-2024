07f54e6fabdd5fa5c234c994f9b1725c6e3076c9
gemaquejr <gemaquejr@hotmail.com>
2023-12-24 01:36:09 -0300

    adiciona caminho de Header em Leaderboard
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/frontend/src/pages/Leaderboard/Leaderboard.jsx
+++ app/frontend/src/pages/Leaderboard/Leaderboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Header from '../../components/Header/Header';
 import LeaderboardTable from '../../components/LeaderboardTable/LeaderboardTable';
 import TableFilter from '../../components/TableFilter';
 
@@ -9,6 +10,12 @@ const Leaderboard = () => {
 
   return (
     <>
+      <Header
+        page="CLASSIFICAÇÃO"
+        FirstNavigationLink={<button />}
+        SecondNavegationLink={<button />}
+      />
+
       <div className={styles.classification_handlers}>
         <TableFilter
           currentFilter={ currentFilter }
