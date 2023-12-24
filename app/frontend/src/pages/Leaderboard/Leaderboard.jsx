1727bb7b422a900fb5534d304d4d88cf143b7323
gemaquejr <gemaquejr@hotmail.com>
2023-12-23 23:59:55 -0300

    adiciona css module em Leaderboard
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/frontend/src/pages/Leaderboard/Leaderboard.jsx
+++ app/frontend/src/pages/Leaderboard/Leaderboard.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import LeaderboardTable from '../../components/LeaderboardTable/LeaderboardTable';
 import TableFilter from '../../components/TableFilter';
 
+import styles from './Leaderboard.module.css';
+
 const Leaderboard = () => {
   const [currentFilter, setCurrentFilter] = useState('Classificação Geral');
 
   return (
     <>
-      <div className="classification-handlers score-board-table-section">
+      <div className={styles.classification_handlers}>
         <TableFilter
           currentFilter={ currentFilter }
           setCurrentFilter={ setCurrentFilter }
