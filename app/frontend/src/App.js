2b2d7a51abb8c20e11cc8de796fc719347f37644
gemaquejr <gemaquejr@hotmail.com>
2024-01-04 14:40:39 -0300

    adiciona caminho de Games em App.js
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import './App.css'
 import Leaderboard from './pages/Leaderboard/Leaderboard';
+import Games from './pages/Games/Games';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route exact path="/" element={ <Navigate to="/leaderboard" /> } />
+          <Route path="/games" element={ <Games /> } />
         </Routes>
       </div>
     </Router>
